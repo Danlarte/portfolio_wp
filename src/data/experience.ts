c0a7a9548154fa73bca8793a17fde6f4ec37ec6c
Daniel Lazarte <daniellazarte@Daniels-MacBook-Pro.local>
2024-11-06 22:17:05 -0300

    initial comit GitHub

---
+++ src/data/experience.ts
@@ -0,0 +1,83 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "Marzo 2020",
+    title: "Diseño y Desarrollo Web",
+    company: "Plural Arquitectos",
+    companyUrl: "https://pluralarquitectos.com.ar/",
+    description:
+      "Diseño y Desarrollo Web: Creación de una interfaz moderna y funcional, adaptada a la estética y necesidades de la firma de arquitectura, optimizando la experiencia de navegación para los usuarios. Optimizacion de velocidad y SEO Básico, Galerias de proyectos e integracion de formulario de contacto.",
+    technologies: [
+      "Ohio Theme",
+      "Google Font API",
+      "Wordpress Plugins",
+      "Contact Form",
+      "WP Backery",
+      "reCAPTCHA",
+      "Affinity Photo"
+    ],
+    logo: "/logos/pluralArquitectos.png",
+  },
+  {
+    dates: "November 2021",
+    title: "Tienda Online",
+    company: "Plutonia",
+    companyUrl: "https://www.plutonia.online",
+    description:
+      "Desarrollo web, Creación de Marca: Diseño de identidad, configuración de Woocommerce, creacion de Custom Hooks para formulario de envios, implementacion de pasarela de pagos segura, desarrollo responsive, optimizacion SEO y velocidad de carga, diseño responsive, automatizacion de pedidos, gestion de correos de confirmacion de compras.",
+    technologies: [
+      "Astra Child Theme",
+      "Google Analytics",
+      "WhatsApp API",
+      "Google Font API",
+      "Yoast Express",
+      "Webp Chat to order",
+      "Mercado Pago Payments",
+      "Woocommerce",
+      "SASS CSS",
+      "Affinity Designer",
+    ],
+    logo: "/logos/plutonia.png",
+  },
+  {
+    dates: "Octubre 2022",
+    title: "Diseño y Desarrollo Web",
+    company: "Ola Comunicación y Género",
+    companyUrl: "https://olacomunicacionygenero.com/",
+    description:
+      "Creación de una interfaz de usuario intuitiva y visualmente atractiva, alineada con los valores y la identidad. Implementación de WordPress para permitir una gestión de contenido sencilla y eficiente.Programación de características específicas para mejorar la funcionalidad del sitio,utilizando PHP para ajustarse a las necesidades de la organización. ",
+    technologies: ["Figma",
+      "Affinity Designer",
+      "Neve Child Theme",
+      "Cloudflare DNS",
+      "Code Snipets",
+      "TranslatePress",
+      "WP Show Posts CPT",
+      "Formidable Form",
+
+
+    ],
+    logo: "/logos/olacomu.png",
+  },
+  {
+    dates: " Junio 2020",
+    title: "Desarrollo Web",
+    company: "Turismo Tastil",
+    companyUrl: "https://www.turismotastil.com.ar/",
+    description:
+      "Se desarrollaron y optimizaron múltiples áreas para ofrecer una plataforma completa y atractiva para la venta de servicios turisticos mayorista y minorista de la region del noroeste argentino. Tambien se configuro un area de reservas, formulario de contacto, area de registro de clientes, CPT y ACF para las tarifas de agencias.",
+    technologies: ["Libreria Moment JS", 
+      "Woocommerce Booking",
+      "Google Maps",
+      "Whatsapp API",
+      "Mercado Pago Payments",
+      "contact Form7",
+      "Code Snipets",
+      "APi iEVT.net",
+      "Sweetalert2",
+    
+    ],
+    logo: "/logos/tastil.png",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,24 @@
+
+
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconBehance from "../components/icons/behance.astro";
+
+
+export const SOCIALS = [
+
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/daniel-lazarte-dev/",
+    icon: IconLinkedin,
+  },
+  
+  {
+    name: "Behance",
+    url: "https://www.behance.net/daniellazarte1",
+    icon: IconBehance,
+  },
+
+
+  
+ 
+];
